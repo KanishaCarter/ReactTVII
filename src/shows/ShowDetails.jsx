9e5fa55d6f909c9b1a3e46f4a0d9568e0e90f020
Kanisha <kanishacarter90@outlook.com>
2025-07-03 20:08:33 -0500

    working on showdetails.jsx

--- src/shows/ShowDetails.jsx
+++ src/shows/ShowDetails.jsx
@@ -1,6 +1,54 @@
+import { useState } from 'react'
+import { tvShows } from './data'
+import { EpisodeDetails } from './EpisodeDetails.jsx'
+import { EpisodeList } from './EpisodeList.jsx'
+
+/*### ShowDetails
+
+1. `<ShowDetails>` takes a `show` as a prop.
+2. `<ShowDetails>` keeps the selected episode in a state variable.
+3. If no show is provided, `<ShowDetails>` renders a message encouraging the user to select a show.
+4. If a show is provided, `<ShowDetails>` renders `<EpisodeList>` and `<EpisodeDetails>`.
+5. `<EpisodeList>` and `<EpisodeDetails>` are provided the correct props. Read the source code of 
+these components to figure out what props they expect!
 import "./shows.css";
 
+export default function EpisodeList({
+name,
+  episodes,
+  selectedEpisode,
+  setSelectedEpisode,
+  
+export default function EpisodeDetails({ episode })
+*/ 
+
 /** Allows users to browse through the episodes of the given show */
-export default function ShowDetails() {
-  return <div className="show-details"></div>;
+export default function ShowDetails({show}) { /*name genre, episodes*/
+
+  const [selectedShow, setSelectedShow] = useState({});
+  
+  return (
+    <div className="show-details">
+      {(!show) ?
+      (<section>
+        <h3>Please select a show.</h3>
+        <h3>Shows List</h3>
+        <ol>
+          {tvShows.map((show)=> (
+            <li 
+            onClick={()=>(setSelectedShow(show))}>
+              {show.name}
+            </li>))}
+        </ol>
+      </section>)
+      :
+      (<h2>{show.name}</h2>
+        <ol>
+          {selectedShow.map((show))}
+        </ol>
+      )}
+    </div>
+  )
+
+
 }
